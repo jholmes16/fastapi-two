e1b9c5b2476ff8055c486a26255b85a091e1a6e7
jholmes16 <jholmesarea@gmail.com>
2022-05-03 11:55:31 -0400

    Add get with variable and all endpoint

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get('/hello')
 def index():
     return {'message': "Hello World"}
+
+# Blog code reads from top to bottom
+@app.get('/blog/all')
+def get_all_blogs():
+    return {'message': 'All blogs provided'}
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'message': f'Blog with id {id}'}
