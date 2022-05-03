2b8b9298819f627cd744042461474d361186a108
jholmes16 <jholmesarea@gmail.com>
2022-05-03 13:50:49 -0400

    add comments to api in swagger

--- main.py
+++ main.py
@@ -13,12 +13,25 @@ def index():
 #def get_all_blogs():
 #    return {'message': 'All blogs provided'}
 
-@app.get('/blog/all', tags = ['blog'])
+@app.get(
+    '/blog/all',
+    tags = ['blog'],
+    summary = 'Retrieve all blogs',
+    description = 'This api call simulates fetching all blogs.'
+    )
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
 @app.get('/blog/{id}/comments/{comment_id}', tags = ['blog', 'comment'])
 def get_comments(id : int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulate retrieving a comment of a blog
+
+    - **id** mandatory path parameters
+    - **comment_id** mandatory path parameters
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
     return{'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 class BlogType(str, Enum):
