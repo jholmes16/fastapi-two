532a98f57cae0176d4c6067f5d9f699979183a45
jholmes16 <jholmesarea@gmail.com>
2022-05-09 12:27:51 -0400

    add create user model and api

---
+++ db/db_user.py
@@ -0,0 +1,17 @@
+import email
+from site import USER_BASE
+from sqlalchemy.orm.session import Session
+from db.hash import Hash
+from schemas import UserBase
+from db.models import DbUser
+
+def create_user(db: Session, request: UserBase):
+  new_user = DbUser(
+    username = request.username,
+    email = request.email,
+    password = Hash.bcrypt(request.password)
+  )
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
+  return new_user
