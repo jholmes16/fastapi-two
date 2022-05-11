9b393419baf02ed3a031374ed51c951f4d81fcf9
jholmes16 <jholmesarea@gmail.com>
2022-05-11 14:09:57 -0400

    make create article and get artiicle by specific id endpoints and article table with relationship

--- db/models.py
+++ db/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 from db.database import Base
 from sqlalchemy import Column
 
@@ -8,3 +10,13 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    items = relationship('DbArticle', back_populates='user')
+
+class DbArticle(Base):
+    __tablename__= 'articles'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    published = Column(Boolean)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("DbUser", back_populates='items')
