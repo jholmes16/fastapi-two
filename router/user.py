c7cbd35f6770079daa270bac21472f7fbacb6e4f
jholmes16 <jholmesarea@gmail.com>
2022-05-11 12:18:33 -0400

    create Update and Delete user endpoints

--- router/user.py
+++ router/user.py
@@ -25,8 +25,12 @@ def get_all_users(db: Session =  Depends(get_db)):
 def get_user(id: int, db: Session = Depends(get_db)):
   return db_user.get_user(db, id)
 
-
 #Update user
-
+@router.post('/{id}/update')
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+  return db_user.update_user(db, id, request)
 
 #Delete user
+@router.get('/delete/{id}')
+def delete(id: int, db: Session = Depends(get_db)):
+  return db_user.delete_user(db, id)
